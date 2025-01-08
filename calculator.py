19894a7cb67160a2c37ba0a94d2580f1f1c3f49c
Gustav Nordqvist <gus.nordqvist@gmail.com>
2025-01-08 14:17:56 -0700

    updates

--- calculator.py
+++ calculator.py
@@ -1,2 +1,23 @@
-def binary_calculator(bin1, bin2, operator):
-    
+# def binary_calculator(bin1, bin2, operator):
+
+# binary_to_decimal
+bin1 = "00000001"
+for i in str(bin1):
+    print(i)
+
+
+
+
+
+
+    # decimal_to_binary = 
+
+    # answer.floor()
+    # if bin2 == "00000000" and operator = "/"
+    # return "NaN"
+
+    # if answer > 256
+    #return "Overflow"
+
+    # if answer < 0
+    # return "Overflow"
