234782edf520e43ffd0dfc708178b224c71eb1a5
Gustav Nordqvist <gus.nordqvist@gmail.com>
2025-01-09 12:39:16 -0700

    updated

--- calculator.py
+++ calculator.py
@@ -1,24 +1,39 @@
 # def binary_calculator(bin1, bin2, operator):
 
 # binary_to_decimal
-bin1 = "00000001"
+operator = "+"
+bin1 = "01000001"
 Error = "Error"
+total = 0
+total2 = 0
 length = len(bin1) - 1
 for index, digit in enumerate(bin1):
-    print(f"Index: {index}, Digit: {digit}, Power: {length - index}, Value: {2**(length - index)}")
-
-    value = {2**(length - index)}
+   # print(f"Index: {index}, Digit: {digit}, Power: {length - index}, Value: {2**(length - index)}")
+    value = 2**(length - index)
     if digit == "0":
      pass
     else:
         if digit == "1":
-          digit = value
+          total = total + value
         else:
           print(Error)
           exit()
 
-print(digit)
+bin2 = "01000001"
+length2 = len(bin2) - 1
+for index2, digit2 in enumerate(bin2):
+    # print(f"Index: {index2}, Digit: {digit2}, Power: {length2 - index2}, Value: {2**(length2 - index2)}")
+    value2 = 2**(length2 - index2)
+    if digit2 == "0":
+     pass
+    else:
+        if digit2 == "1":
+          total2 = total2 + value2
+        else:
+          print(Error)
+          exit()
 
+print(total, operator, total2)
 
     # if digit is 1, add "Value ^" to a total, else if 0 do nothing, else return "Error"
 
